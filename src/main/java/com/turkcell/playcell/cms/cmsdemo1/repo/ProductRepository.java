f9e0052b0ab6e522da9c4f7b77929b59ada69e6a
EXT02D17169 <halil.cetinkaya@consultant.turkcell.com.tr>
2019-01-21 10:31:17 +0300

    bug fix

--- src/main/java/com/turkcell/playcell/cms/cmsdemo1/repo/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.turkcell.playcell.cms.cmsdemo1.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
